cb283ffb215fecfe819a796fe52eeebc721dcde4
Shan Wijenayaka <shanwijenayaka@outlook.com>
2021-05-17 18:19:40 +0530

    spring boot initialization with reactive web

---
+++ src/main/java/com/shanwije/backpacker/BackpackerApplication.java
@@ -0,0 +1,13 @@
+package com.shanwije.backpacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackpackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shanwije/backpacker/BackpackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanwije.backpacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackpackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
