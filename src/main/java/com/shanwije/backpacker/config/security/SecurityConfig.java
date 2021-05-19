8e288b5b619a9052c74aad6756b1d8844b1f3b28
Shan Wijenayaka <shanwijenayaka@outlook.com>
2021-05-19 23:07:09 +0530

    basic authentication added

--- src/main/java/com/shanwije/backpacker/config/security/SecurityConfig.java
+++ src/main/java/com/shanwije/backpacker/config/security/SecurityConfig.java
@@ -1,21 +1,55 @@
 package com.shanwije.backpacker.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
-//@EnableWebFluxSecurity
+@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
-//    @Bean
-//    MapReactiveUserDetailsService userDetailsService(){
-//        UserDetails rob = User.builder()
-//        .username("shan@x.com")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//        return new MapReactiveUserDetailsService(rob);
-//    }
+    @Value("${spring.security.basic.auth.username}")
+    String username;
+    @Value("${spring.security.basic.auth.password}")
+    CharSequence password;
+    @Value("${spring.security.basic.auth.roles}")
+    String roles;
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService(){
+
+        UserDetails userDetails = User.withUsername(username)
+                .password(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(password))
+                .roles(roles)
+                .build();
+
+        return new MapReactiveUserDetailsService(userDetails);
+    }
+
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange(
+                authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated()
+        ).exceptionHandling()
+                .authenticationEntryPoint((res, err) -> Mono.fromRunnable(()-> {
+                    res.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                })).accessDeniedHandler((res,err) -> Mono.fromRunnable(() -> {
+                    res.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+                })).and()
+                .httpBasic((Customizer.withDefaults()))
+                .formLogin().disable()
+                .csrf().disable()
+                .build();
+    }
 }
--- src/main/java/com/shanwije/backpacker/controller/UserController.java
+++ src/main/java/com/shanwije/backpacker/controller/UserController.java
@@ -5,10 +5,13 @@ import com.shanwije.backpacker.service.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.awt.*;
@@ -18,31 +21,46 @@ import java.awt.*;
 @RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
-    @Autowired
+    final
     UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping()
-    public Publisher<User> getAll() {
+    public Flux<User> getAll() {
         return userService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Publisher<User> getById(@PathVariable(value = "id")String id) {
-        return userService.findById(id);
+    public Mono<ResponseEntity<User>> getById(@PathVariable(value = "id")String id) {
+        return userService.findById(id)
+                .map(user -> ResponseEntity.ok(user))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Publisher<User> create(@RequestBody User user) {
+    public Mono<User> create(@RequestBody User user) {
         return userService.save(user);
     }
 
     @DeleteMapping
-    public Publisher<User> deleteById(@PathVariable(value = "id")String id) {
-        return userService.delete(id);
+    public Mono<ResponseEntity<User>> deleteById(@PathVariable(value = "id")String id) {
+        return userService.delete(id).map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PutMapping
-    public Publisher<User> updateUser(@RequestBody User user) {
-        return userService.save(user);
+    public Mono<ResponseEntity<User>> updateUser(@PathVariable(value = "id")String id,
+                                      @RequestBody User user) {
+        return userService.findById(id)
+                .flatMap(user1 -> {
+                    user1.setEmail(user.getEmail());
+                    user1.setPassword(user.getPassword());
+                    return userService.save(user1);
+                })
+                .map(updatedUser -> new ResponseEntity<>(updatedUser, HttpStatus.OK))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
